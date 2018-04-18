231f6f99b6f83f41dee08fbfd1b3dd45fcbe1fa7
Mudartastisch <kailorenz96@aol.com>
2018-04-18 15:47:03 +0200

    done task a
    Eclipse eGit does not work properly. Uploading changes by hand

--- FileRequestHandler.java
+++ FileRequestHandler.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
-
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 /**
  * Request handler for HTTP/1.1 GET requests.
@@ -10,6 +12,7 @@ public class FileRequestHandler {
 
     private final Path documentRoot;
     private static final String NEW_LINE = System.lineSeparator();
+    private String[] args;
 
     public FileRequestHandler(Path documentRoot) {
         this.documentRoot = documentRoot;
@@ -28,9 +31,14 @@ public class FileRequestHandler {
      */
     public void handle(String request, OutputStream response)
     throws IOException {
-        response.write("Method handle not implemented.".getBytes());
-        response.write(NEW_LINE.getBytes());
-
+    	args = request.split(" ");
+    	Path path = Paths.get(args[1]);
+    	response.write(Arrays.toString(args).getBytes());
+    	if(!(args.length == 3)) {response.write(status_message(400).getBytes()); return;}
+    	if(!Files.exists(path)) {response.write(status_message(404).getBytes()); return;}
+    	if(!(args[0].equals("GET"))) {response.write(status_message(501).getBytes()); return;}
+    	if(!(args[2].equals("HTTP/1.1"))) {response.write(status_message(505).getBytes()); return;}
+    	
         /*
          * (a) Determine status code of the request and write proper status
          * line to the response output stream.
@@ -44,5 +52,16 @@ public class FileRequestHandler {
          *
          * … to the response output stream.
          */
+    }
+    //Map status message to status code
+    public String status_message(int code) {
+    	switch(code) {
+    	case 200: return "\nHTTP/1.1 200 OK\n";
+    	case 400: return "\nHTTP/1.1 400 Bad Request\n";
+    	case 404: return "\nHTTP/1.1 404 Not found\n";
+    	case 501: return "\nHTTP/1.1 501 Not implemented\n";
+    	case 505: return "\nHTTP/1.1 505 HTTP Version not supported\n";
+    	default: return "\nUnkown Error\n";
+    	}
     }
 }
