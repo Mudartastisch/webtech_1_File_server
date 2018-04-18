36336e5123d59248995df36f35a041b9bb1867ef
Mudartastisch <kailorenz96@aol.com>
2018-04-18 16:36:12 +0200

    Trying b, filepath not working
    I need to find a way to combine paths because the index.html is not the in same sub folder as the server

--- FileRequestHandler.java
+++ FileRequestHandler.java
@@ -4,6 +4,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Request handler for HTTP/1.1 GET requests.
@@ -13,6 +16,7 @@ public class FileRequestHandler {
     private final Path documentRoot;
     private static final String NEW_LINE = System.lineSeparator();
     private String[] args;
+    private String buffer;
 
     public FileRequestHandler(Path documentRoot) {
         this.documentRoot = documentRoot;
@@ -31,14 +35,36 @@ public class FileRequestHandler {
      */
     public void handle(String request, OutputStream response)
     throws IOException {
+    	//task a
     	args = request.split(" ");
     	Path path = Paths.get(args[1]);
+    	Path http_root = Paths.get("/www-root"+args[1]);
     	response.write(Arrays.toString(args).getBytes());
+    	response.write(Paths.get("http_root").normalize().toString().getBytes());
+    	response.write(Boolean.toString(Files.exists(http_root)).getBytes());
     	if(!(args.length == 3)) {response.write(status_message(400).getBytes()); return;}
-    	if(!Files.exists(path)) {response.write(status_message(404).getBytes()); return;}
+    	if(!Files.exists(path) || !Files.exists(http_root)) {response.write(status_message(404).getBytes()); return;}
     	if(!(args[0].equals("GET"))) {response.write(status_message(501).getBytes()); return;}
     	if(!(args[2].equals("HTTP/1.1"))) {response.write(status_message(505).getBytes()); return;}
     	
+    	//task b
+    	if(Files.exists(http_root)) {path = http_root;};
+    	response.write(status_message(200).getBytes());
+    	
+    	buffer = "DATE: "+getHttpDate()+"\n";
+    	response.write(buffer.getBytes());
+    	
+    	buffer = "Content-Type: "+Files.probeContentType(path);
+    	response.write(buffer.getBytes());
+    	
+    	buffer = "Content-Length: "+Long.toString(Files.size(path));
+    	response.write(buffer.getBytes());
+    	
+    	buffer = "Last-Modified: "+Files.getLastModifiedTime(path).toString();
+    	response.write(buffer.getBytes());
+    	
+    	buffer = Arrays.toString(Files.readAllBytes(path));
+    	response.write(buffer.getBytes());
         /*
          * (a) Determine status code of the request and write proper status
          * line to the response output stream.
@@ -63,5 +89,12 @@ public class FileRequestHandler {
     	case 505: return "\nHTTP/1.1 505 HTTP Version not supported\n";
     	default: return "\nUnkown Error\n";
     	}
+    }
+    public String getHttpDate() {
+    // Proper date format as per RFC 2616 (HTTP/1.1):
+    // https://tools.ietf.org/html/rfc2616#section-3.3.1
+    	String httpDateFormat = "EEE, dd MMM yyyy HH:mm:ss zzz";
+    	DateFormat dateFormat = new SimpleDateFormat(httpDateFormat);
+    	return dateFormat.format(new Date());
     }
 }
